1eeed030bc2f57c77f46de8b0cd08bfa422b9d70
Abid Hussain <hussainabid7199@gmail.com>
2023-04-08 01:09:30 +0530

    Pkgjson index file updated to deploy

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+
+
+dotenv.config();
+require("./db/connection");
+require("./model/schema");
+// for parsing application/json
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+
+
+
+// route
+app.use(require("./router/auth"));
+
+
+
+let port = process.env.PORT;
+
+
+
+app.listen(port, (req, res) => {
+    console.log(`Server is running on ${port}`);
+})
+
