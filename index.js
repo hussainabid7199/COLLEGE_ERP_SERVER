e9112e0a702df300ef73330d23e78ae22b7893ed
Abid Hussain <hussainabid7199@gmail.com>
2023-04-30 23:54:13 +0530

    Access-control-allow-origin-revert

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cors());
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 app.use(cookieParser());
 
 require("./db/connection");
@@ -19,11 +24,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 app.get("/", (req, res) => {
     res.send("Welcome to ADDAX SOLUTION Ltd.")
 })
