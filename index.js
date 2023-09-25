fc27962994e74df6ca4d89b490756245c8122488
Abid Hussain <79327149+hussainabid7199@users.noreply.github.com>
2023-09-25 22:38:35 +0530

    Update cors

--- index.js
+++ index.js
@@ -8,28 +8,17 @@ const cookieParser = require("cookie-parser");
 
 
 const app = express();
-var allowlist = [process.env.BASE_URL,  process.env.BASE_PORT]
-const corsOptions = {
-    origin: `${allowlist} `, // specify the allowed origin(s)
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'], // specify the allowed HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // specify the allowed headers
-    credentials: true, // allow credentials (cookies, HTTP authentication) to be included
-  };
-
-app.use(cors(corsOptions));
 app.use(cookieParser());
 
-// const whitelist = [process.env.BASE_URL, 'http://developer2.com']
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error())
-//     }
-//   }
-// }
 
+app.use(cors({
+    "origin": [process.env.BASE_URL, process.env.BASE_PORT, process.env.STAGING_URL],
+    "methods": ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+    "preflightContinue": true,
+    "optionsSuccessStatus": 204,
+    "allowedHeaders": ['Content-Type','Authorization'],
+    "credentials": true
+}));
 
 
 require("./db/connection");
